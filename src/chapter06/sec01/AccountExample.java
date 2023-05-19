2cffe704927322b674ddf87667f18acd7136a782
syangfa <kjp0602@gmail.com>
2023-05-19 18:36:09 +0900

    chapter06 sec1 commit

--- src/chapter06/sec01/AccountExample.java
+++ src/chapter06/sec01/AccountExample.java
@@ -8,17 +8,17 @@ public class AccountExample {
     }
 
     public void start() {
-        Account account=new Account("홍길동");
+        Account account = new Account("홍길동");
         account.deposit(10000);
         printInfo(account);                  // account 객체를 printInfo에게 전달 해줌
-        int amt=account.withdraw(5000);
-        System.out.println("출금 금액 : "+amt);
+        int amt = account.withdraw(5000);
+        System.out.println("출금 금액 : " + amt);
         printInfo(account);
     }
 
     public void printInfo(Account account) {   //account 객체를 start에게서 전달 받음
-        System.out.println("계좌주 : "+account.getAccountOwner());
-        System.out.println("잔고 : "+account.getBalance());
+        System.out.println("계좌주 : " + account.getAccountOwner());
+        System.out.println("잔고 : " + account.getBalance());
     }
 
 }
